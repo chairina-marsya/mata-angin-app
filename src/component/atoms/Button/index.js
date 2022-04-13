8332536aa67af62800a60d62d662b0190f102f3f
Chairina Marsya <chairina.m@photon.in>
2022-04-14 01:41:26 +0700

    fix

--- src/component/atoms/Button/index.js
+++ src/component/atoms/Button/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 export default function ButtonAtom({
-=======
     id,
     variant, //outlined,contained,text
     color, //primary,secondary,success,error,info,warning,string
